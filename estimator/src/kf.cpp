7c51bda83a5c227f3d2c4f931d9a94c02cde841c
Akash_Ravindra_Laptop <akash.ravindra@gmail.com>
2023-05-05 02:16:49 -0400

    updated to seed using first 2 mes

--- estimator/src/kf.cpp
+++ estimator/src/kf.cpp
@@ -18,57 +18,64 @@ KF::KF() : KF{defaultP, defaultR, defaultH} {};
 
 KF::KF(const double &gravity) : KF{defaultP, defaultR, defaultH, gravity} {};
 
-void KF::updatedt_(const double &dt){
+void KF::updatedt_(const double &dt) {
   this->dt_.push_back(dt);
   if (this->dt_.size() > 10) {
     this->dt_.erase(this->dt_.begin());
   }
 }
 
-
-void KF::updateParams_(const double &dt){
-    this->Q_ = this->get_Q_(dt);
+void KF::updateParams_(const double &dt) {
+  this->Q_ = this->get_Q_(dt);
   this->A_ = this->get_A_(dt);
   this->B_ = this->get_B_(dt);
 }
-void KF::updateParams_(){
-    this->updateParams_(this->dt_.back());
-}
-
+void KF::updateParams_() { this->updateParams_(this->dt_.back()); }
 
 void KF::step(const double &dt, const Matrix<double, 3, 1> &z) {
-    this->updatedt_(dt);
-    this->updateParams_();
+  if (this->history_.size() < 2) {
+    // Initialize the history and use as seed for states
+    this->history_.push_back(z);
+    if (this->history_.size() == 2) {
+      this->x_.row(0) = this->history_[1];
+      this->x_.row(3) = (this->history_[1] - this->history_[0]) / dt;
+    }
+    return;
+  }
+  this->updatedt_(dt);
+  this->updateParams_();
   // Predict
   this->predict(dt);
   // Update
   this->update(z);
 };
 
-void KF::step(){
-    this->updateParams_(0.0001);
-    // Predict
-    this->predict(0.0001);
-    // Update
-    this->x_ = this->x_hat_;
-    this->P_ = this->P_hat_;
+void KF::step() {
+  this->updateParams_(0.0001);
+  // Predict
+  this->predict(0.0001);
+  // Update
+  this->x_ = this->x_hat_;
+  this->P_ = this->P_hat_;
 };
 
 void KF::update(const Matrix<double, 3, 1> &z) {
   // Calculate Kalman gain
-  this->K_ = this->P_hat_ * this->H_.transpose() *
-             (this->H_ * this->P_hat_ * this->H_.transpose() + this->R_).inverse();
-    // Update state estimate
-    this->x_ = this->x_hat_ + this->K_ * (z - this->H_ * this->x_hat_);
-    // Update covariance estimate
-    this->P_ = (Matrix<double, 6, 6>::Identity() - this->K_ * this->H_) * this->P_hat_ *
-               (Matrix<double, 6, 6>::Identity() - this->K_ * this->H_).transpose() +
-               this->K_ * this->R_ * this->K_.transpose();
+  this->K_ =
+      this->P_hat_ * this->H_.transpose() *
+      (this->H_ * this->P_hat_ * this->H_.transpose() + this->R_).inverse();
+  // Update state estimate
+  this->x_ = this->x_hat_ + this->K_ * (z - this->H_ * this->x_hat_);
+  // Update covariance estimate
+  this->P_ =
+      (Matrix<double, 6, 6>::Identity() - this->K_ * this->H_) * this->P_hat_ *
+          (Matrix<double, 6, 6>::Identity() - this->K_ * this->H_).transpose() +
+      this->K_ * this->R_ * this->K_.transpose();
 };
 
 void KF::predict(const double &dt) {
-    // Update state estimate
-    this->x_hat_ = this->A_ * this->x_ + this->B_ * this->u_;
-    // Update covariance estimate
-    this->P_hat_ = this->A_ * this->P_ * this->A_.transpose() + this->Q_;
+  // Update state estimate
+  this->x_hat_ = this->A_ * this->x_ + this->B_ * this->u_;
+  // Update covariance estimate
+  this->P_hat_ = this->A_ * this->P_ * this->A_.transpose() + this->Q_;
 }
