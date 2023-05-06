cd6f25077a939e0a37cd8ca295f5b4b3d199d64e
Akash_Ravindra_Laptop <akash.ravindra@gmail.com>
2023-05-06 13:22:43 -0400

    Estimator action server and the runner script

--- estimator/src/runner.cpp
+++ estimator/src/runner.cpp
@@ -1,3 +1,9 @@
 #include "../include/estimator/estimator.hpp"
 
-int main() { return 0; }
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "estimator");
+  et::Estimator estimator(ros::this_node::getName());
+  ros::spin();
+  ROS_INFO("Exiting the estimator node");
+  return 0;
+}
