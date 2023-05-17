448a5ce94cfe6a90c58918bf3a0fd8f6d7f29901
Akash_Ravindra_Laptop <akash.ravindra@gmail.com>
2023-05-17 18:17:22 -0400

    Modified waypoints to have shorter takeoff and longer timeout

--- flyer/src/waypoint.cpp
+++ flyer/src/waypoint.cpp
@@ -1,6 +1,6 @@
 #include "../include/flyer/waypoint.hpp"
 
-const std::pair<Eigen::Vector3d, Eigen::Vector3d> Waypoints::TAKEOFF = std::make_pair(Eigen::Vector3d(0, 0, 1.0), Eigen::Vector3d(0, 0, 0));
+const std::pair<Eigen::Vector3d, Eigen::Vector3d> Waypoints::TAKEOFF = std::make_pair(Eigen::Vector3d(0, 0, 0.75), Eigen::Vector3d(0, 0, 0));
 const std::pair<Eigen::Vector3d, Eigen::Vector3d> Waypoints::LAND = std::make_pair(Eigen::Vector3d(0, 0, 0.2), Eigen::Vector3d(0, 0, 0));
 const std::pair<Eigen::Vector3d, Eigen::Vector3d> Waypoints::HOME = Waypoints::TAKEOFF;
 std::pair<Eigen::Vector3d, Eigen::Vector3d> Waypoints::MOVE = std::make_pair(Eigen::Vector3d(0, 0, 0), Eigen::Vector3d(0, 0, 0));
@@ -11,4 +11,4 @@ constexpr double Waypoints::minX = -2.0;    // meters
 constexpr double Waypoints::minY = -2.0;    // meters
 constexpr double Waypoints::minZ = 0;        // meters
 constexpr double Waypoints::tolerance = 0.2; // meters
-constexpr double Waypoints::timeout = 5.0; // seconds
+constexpr double Waypoints::timeout = 6.0; // seconds
